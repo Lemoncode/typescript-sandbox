516a8797e5e22c557be6622c8dd77f5bf659b470
Braulio Diez Botella <braulio.diez@gmail.com>
2023-04-02 13:26:33 +0200

    added test support

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,14 @@
 import { defineConfig } from "vite";
 import checker from "vite-plugin-checker";
+import type { UserConfig as VitestUserConfigInterface } from "vitest/config";
+
+const vitestConfig: VitestUserConfigInterface = {
+  test: {
+    globals: true,
+  },
+};
 
 export default defineConfig({
   plugins: [checker({ typescript: true })],
+  test: vitestConfig.test,
 });
