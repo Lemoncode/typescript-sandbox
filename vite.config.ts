475bfaccde61aab21f0938d33595f1576ab6fc14
Antonio Contreras <antoniocontrerasroman@gmail.com>
2025-09-18 13:13:22 +0200

    update typescript version

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,6 @@
 import { defineConfig } from "vite";
 import checker from "vite-plugin-checker";
-import type { UserConfig as VitestUserConfigInterface } from "vitest/config";
-
-const vitestConfig: VitestUserConfigInterface = {
-  test: {
-    globals: true,
-    restoreMocks: true,
-  },
-};
 
 export default defineConfig({
   plugins: [checker({ typescript: true })],
-  test: vitestConfig.test,
 });
